308ad58466862095f1cd41ab8b36c82a42e3489e
Omar <martinez.trujillo.omar@gmail.com>
2021-02-09 20:57:54 +0100

    first commit

---
+++ src/main/java/com/omar/App.java
@@ -0,0 +1,12 @@
+package com.omar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("TEst");
+    }
+}
