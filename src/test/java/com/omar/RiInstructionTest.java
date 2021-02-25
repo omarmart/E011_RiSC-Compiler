5cc7da93c1bac9bad12482a8a05185868edc6318
Omar <martinez.trujillo.omar@gmail.com>
2021-02-25 22:22:34 +0100

    add tests to RiInstruction

---
+++ src/test/java/com/omar/RiInstructionTest.java
@@ -0,0 +1,34 @@
+package com.omar;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class RiInstructionTest {
+    @Test
+    public void shouldConstruct() {
+        RiInstruction inst = new RiInstruction(OperationCode.LUI, Register.R1, 2);
+
+        assertEquals("Failed to set OperationCode", OperationCode.LUI, inst.getOpCode());
+        assertEquals("Failed to set Register", Register.R1, inst.getRegisterA());
+        assertEquals("Failed to set immediate", 2, inst.getImmediate());
+    }
+
+    @Test
+    public void shouldParsetoHex() {
+        RiInstruction inst = new RiInstruction(OperationCode.LUI, Register.R0, 7);
+        byte[] instHex = inst.parseToHex();
+        // 011 000 0000000111
+        // 0110 0000 0000 0111
+        //  6   0   0   7
+        assertArrayEquals("Failed to parse to Hex", new byte[] { 0x60, 0x07 }, instHex);
+    }
+
+    @Test
+    public void shouldParseToString() {
+        RiInstruction inst = new RiInstruction(OperationCode.LUI, Register.R4, 11);
+        String instString = inst.toString();
+        assertEquals("Failed to parse to String", "lui 4 11", instString);
+    }
+}
